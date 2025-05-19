4167d22481eebf2558ab10f5cc2f43998f6a9257
Angel Cabrera Martos <cabreramartosa96@gmail.com>
2025-05-19 19:28:20 +0200

    proyecto realizado Angel Cabrera Martos

---
+++ backend/src/Controller/RECUAController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class INIController
+{
+#[Route('/api/RECUA', name: 'api_recua')]
+public function index(): Response
+{
+return new Response('Symfony de Angel Cabrera Martos Responde a la llamada de React');
+}
+}
